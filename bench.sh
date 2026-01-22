23d90df784c107dcd946902aa984e3fa40333185
i5heu <i5heu@i5h.eu>
2026-01-22 19:22:50 +0000

    Refactor code structure for improved readability and maintainability

--- bench.sh
+++ bench.sh
@@ -1,1 +1,3 @@
-go run cmd/bench/main.go -json -high-concurrency -iter 3 && go run cmd/buildGraph/main.go && python3 analyze.py
+go run cmd/bench/main.go -json -iter 1 && go run cmd/buildGraph/main.go && python3 analyze.py
+
+#go run cmd/bench/main.go -json -high-concurrency -iter 3 && go run cmd/buildGraph/main.go && python3 analyze.py
